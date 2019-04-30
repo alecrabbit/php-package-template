009828caaebe31e7733d59f8d837d0ca1f438bc6
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-30 13:38:27 +0300

    src

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class BasicTest extends TestCase
 {
     /** @test */
-    public function Dummy():void
+    public function dummy():void
     {
         $this->assertTrue(true);
     }
