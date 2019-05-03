48b399cfc5b2b6523512267bdefe0448ba5a84ab
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-03 13:17:59 +0300

    move result

--- bin/settings.sh
+++ bin/settings.sh
@@ -102,13 +102,13 @@ TITLE_FILE="./../${TERMINAL_TITLE_FILE}"
 ENV_FILE=".env"
 MAIN_ENV_FILE="../${ENV_FILE}"
 HEADER="$(basename $(dirname "$(pwd)"))"
-COMPOSER_JSON_FILE="composer.json.result";
+PKG_SETTINGS_DIR=".settings";
+COMPOSER_JSON_FILE="${PKG_SETTINGS_DIR}/composer.json.result";
 COMPOSER_JSON_TEMPLATE="template.composer.json";
 GIT_ATTR_FILE=".gitattributes.dist";
 DIST_ENV_FILE=".env.dist";
 DIST_DEFAULTS=".defaults.dist";
 GLOBAL_DIST_DEFAULTS="${HOME}/.php-package-template.defaults";
-PKG_SETTINGS_DIR=".settings";
 
 if [[ ${COVERAGE} == 1 ]]
 then
