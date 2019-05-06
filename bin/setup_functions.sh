54e600dc5b6f5d518eca7c3b2084124355938d06
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-06 15:50:32 +0300

    help message

--- bin/setup_functions.sh
+++ bin/setup_functions.sh
@@ -3,13 +3,13 @@
 setup_help_message () {
 if [[ ${HELP} == 1 ]]
 then
+    echo "Options:"
+    echo "    --help               - show this message"
     echo "Usage:"
     echo "./setup name owner \"Your Name\""
-    echo "name                   - package name"
-    echo "owner                  - package owner"
-    echo "\"Your Name\"          - package owner name"
-    echo "Options:"
-    echo "  --help               - show this message"
+    echo "    name                   - package name"
+    echo "    owner                  - package owner"
+    echo "    \"Your Name\"            - package owner name"
     exit 0
 fi
 }
