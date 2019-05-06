141c39844e135cf63690b435079418bbad164bdb
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-06 16:10:41 +0300

    add origin

--- bin/setup_functions.sh
+++ bin/setup_functions.sh
@@ -14,3 +14,22 @@ then
 fi
 }
 
+add_origin () {
+    param_package_owner="$1"
+    param_package_name="$2"
+    remote="https://github.com/${param_package_owner}/${param_package_name}.git"
+    if [[ $(ask_question "Add remote on '${remote}'?") == 1 ]]
+    then
+        git remote add origin "https://github.com/${param_package_owner}/${param_package_name}.git"
+    fi
+}
+
+ask_question () {
+    read -p "$(dark "Timeout 10 seconds...") ${1}" -n 1 -r -t 10
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        return 1
+    fi
+    return 0
+}
